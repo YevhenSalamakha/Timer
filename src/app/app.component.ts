1e7f01b433b74567ee7d894315f796fba8e50d80
Yevhenii <80651146+YevhenSalamakha@users.noreply.github.com>
2022-06-28 15:15:13 +0300

    done with layout and styles for my app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'TimerTask';
+ 
+
+  constructor() {
+  
+  }
 }
